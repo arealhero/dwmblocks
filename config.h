4ed4ab0d14b6071cc903eda37ade8d4d41e98081
Vladislav Sharshukov <vsharshukov@gmail.com>
2023-05-17 17:01:47 +0300

    Scripts added & config cleaned up

--- config.h
+++ config.h
@@ -1,9 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "sb-battery",	5,		3},
-	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+  /*Icon*/  /*Command*/    /*Update Interval*/  /*Update Signal*/
+  { "", "sb-xkblayout",    0,    10 },
+  { "", "sb-volume",       0,    11 },
+  { "", "sb-battery",      5,    3  },
+  { "", "sb-ram",          30,   0  },
+  { "", "sb-date",         5,    0  },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
